ffe0bfd832b7b1a23f62c674f483e2479889495a
Kevin Alavik <kevin@alavik.se>
2024-03-04 16:22:38 +0100

    [BROKEN] Some weird font issue???

--- src/core/browser.c
+++ src/core/browser.c
@@ -125,38 +125,84 @@ typedef struct
 
 render_node render_array[64];
 
+int cury = 1;          // Make this system better later on.
+#define ELEM_HEIGTH 72 //
+
+// Move this somewhere later on (This is a function to strcmp without any
+// restrctions on the case)
+int
+priv_stricmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s2) {
+        char c1 = tolower(*s1);
+        char c2 = tolower(*s2);
+        if (c1 != c2) {
+            return c1 - c2;
+        }
+        s1++;
+        s2++;
+    }
+    return tolower(*s1) - tolower(*s2);
+}
+
 static void
 render_element(struct parse_element *element)
 {
-    const char *element_content = get_element_content(element);
-    int element_content_length = strlen(element_content) + 1;
+    if ((priv_stricmp(element->name, "h1") == 0) ||
+        (priv_stricmp(element->name, "h2") == 0) ||
+        (priv_stricmp(element->name, "h3") == 0) ||
+        (priv_stricmp(element->name, "h4") == 0) ||
+        (priv_stricmp(element->name, "h5") == 0) ||
+        (priv_stricmp(element->name, "h6") == 0) ||
+        (priv_stricmp(element->name, "p") == 0)) {
 
-    SDL_Color foreground_color = { 0, 0, 0 };
-    // TODO: implement parse_attribute based color setting
+        const char *element_content = get_element_content(element);
+        if (element_content == NULL) {
+            return;
+        }
 
-    render_array[1].text = malloc(sizeof(char) * element_content_length);
-    snprintf(render_array[1].text, element_content_length, element_content);
-    render_array[1].x = 0;
-    render_array[1].y = 72;
-    render_array[1].width = -1;
-    render_array[1].height = 37;
-    render_array[1].color = foreground_color;
-    render_array[1].font = current_font_sansserif;
+        int element_content_length = strlen(element_content) + 1;
+
+        SDL_Color foreground_color = { 0, 0, 0 };
+
+        render_array[1].text = malloc(sizeof(char) * element_content_length);
+        snprintf(render_array[1].text, element_content_length, element_content);
+        render_array[1].x = 0;
+        render_array[1].y = 72;
+        render_array[1].width = -1;
+        render_array[1].height = 37;
+        render_array[1].color = foreground_color;
+        render_array[1].font = current_font_sansserif;
+
+        log_debug("Rendered an \"%s\". Content: \"%s\"",
+                  element->name,
+                  element->content);
+        cury++;
+    }
+}
+
+static void
+render_all_elements_from_tree(struct parse_node *tree)
+{
+    if (tree == NULL) {
+        log_error("Tried rendering empty HTML tree!");
+        return;
+    }
+
+    if (tree->element != NULL) {
+        render_element(tree->element);
+    }
+
+    for (int i = 0; i < tree->num_children && tree->children != NULL; i++) {
+        render_all_elements_from_tree(tree->children[i]);
+    }
 }
 
 static void
 render_html(struct parse_node *tree)
 {
     log_debug("Rendering HTML content");
-
-    if (tree == NULL) {
-        log_error("Tried rendering empty HTML tree!");
-        return;
-    }
-
-    struct parse_element *element = find_in_html_tree(tree, "h1");
-
-    render_element(element);
+    render_all_elements_from_tree(tree);
 }
 
 static void
@@ -230,7 +276,7 @@ browser_init()
     window = suzwin_create_window(1280, 720, 0, "");
     suzwin_set_icon(window, "../res/logo.png");
 
-    load_page("http://info.cern.ch/hypertext/WWW/TheProject.html");
+    load_page("http://localhost/heading.html");
     return true;
 }
 
