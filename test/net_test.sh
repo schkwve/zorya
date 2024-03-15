74b5daccb331ba068c5d0c1e5cf92d355f87a837
lolguy91 <retek1544@gmail.com>
2024-03-15 10:45:55 +0000

    deleted useless test files

--- test/net_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -D__DEBUG -g -fsanitize=address  -o test/net_test test/net_test.c src/utils/buffer.c src/utils/logging.c src/netwerk/connect.c src/netwerk/http.c && ./test/net_test && rm ./test/net_test
